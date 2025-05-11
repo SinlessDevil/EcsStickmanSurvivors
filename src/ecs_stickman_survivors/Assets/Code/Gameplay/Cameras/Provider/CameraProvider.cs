d48e6438a35081db43179834ccacf6418491e239
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-05-11 10:45:40 +0300

    add: new follow camera

--- src/ecs_stickman_survivors/Assets/Code/Gameplay/Cameras/Provider/CameraProvider.cs
+++ src/ecs_stickman_survivors/Assets/Code/Gameplay/Cameras/Provider/CameraProvider.cs
@@ -2,26 +2,29 @@
 
 namespace Code.Gameplay.Cameras.Provider
 {
-  public class CameraProvider : ICameraProvider
-  {
-    public Camera MainCamera { get; private set; }
-
-    public float WorldScreenHeight { get; private set; }
-    public float WorldScreenWidth { get; private set; }
-
-    public void SetMainCamera(Camera camera)
+    public class CameraProvider : ICameraProvider
     {
-      MainCamera = camera;
+        public Camera MainCamera { get; private set; }
 
-      RefreshBoundaries();
-    }
+        public float WorldScreenHeight { get; private set; }
+        public float WorldScreenWidth { get; private set; }
+        public Vector3 CameraStartPosition { get; private set; }
 
-    private void RefreshBoundaries()
-    {
-      Vector2 bottomLeft = MainCamera.ViewportToWorldPoint(new Vector3(0, 0, MainCamera.nearClipPlane));
-      Vector2 topRight = MainCamera.ViewportToWorldPoint(new Vector3(1, 1, MainCamera.nearClipPlane));
-      WorldScreenWidth = topRight.x - bottomLeft.x;
-      WorldScreenHeight = topRight.y - bottomLeft.y;
+        public void SetMainCamera(Camera camera)
+        {
+            MainCamera = camera;
+
+            CameraStartPosition = MainCamera.transform.position;
+            
+            RefreshBoundaries();
+        }
+
+        private void RefreshBoundaries()
+        {
+            Vector2 bottomLeft = MainCamera.ViewportToWorldPoint(new Vector3(0, 0, MainCamera.nearClipPlane));
+            Vector2 topRight = MainCamera.ViewportToWorldPoint(new Vector3(1, 1, MainCamera.nearClipPlane));
+            WorldScreenWidth = topRight.x - bottomLeft.x;
+            WorldScreenHeight = topRight.y - bottomLeft.y;
+        }
     }
-  }
 }
--- src/ecs_stickman_survivors/Assets/Code/Gameplay/Cameras/Provider/ICameraProvider.cs
+++ src/ecs_stickman_survivors/Assets/Code/Gameplay/Cameras/Provider/ICameraProvider.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 namespace Code.Gameplay.Cameras.Provider
 {
-  public interface ICameraProvider
-  {
-    Camera MainCamera { get; }
-    float WorldScreenHeight { get; }
-    float WorldScreenWidth { get; }
-    void SetMainCamera(Camera camera);
-  }
+    public interface ICameraProvider
+    {
+        Camera MainCamera { get; }
+        float WorldScreenHeight { get; }
+        float WorldScreenWidth { get; }
+        Vector3 CameraStartPosition { get; }
+        void SetMainCamera(Camera camera);
+    }
 }
