9b65a7a80dc5bf834eb70efa07004fecd6b72aac
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-05-13 12:50:36 +0300

    feat: new generator

--- src/ecs_stickman_survivors/Assets/Code/Editors/ColliderMeshCreator.cs
+++ src/ecs_stickman_survivors/Assets/Code/Editors/ColliderMeshCreator.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Plugins.ConcaveHullGenerator;
 using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
 using UnityEditor;
@@ -27,6 +28,14 @@ namespace Code.Editors
         [BoxGroup("Collider Mesh Generation")] [LabelText("Debug Material")]
         public Material debugMaterial;
 
+        [BoxGroup("Collider Mesh Generation")] [LabelText("Concavity (-1 to 1)")]
+        [Range(-1f, 1f)]
+        public float concavity = 0.5f;
+
+        [BoxGroup("Collider Mesh Generation")] [LabelText("Scale Factor")]
+        [MinValue(0.01f)]
+        public float scaleFactor = 1f;
+        
         [BoxGroup("Collider Mesh Generation")]
         [Button(ButtonSizes.Large)]
         [GUIColor(0.3f, 0.9f, 0.4f)]
@@ -47,7 +56,12 @@ namespace Code.Editors
                 return;
             }
 
-            var edgePoints = ExtractMeshOutlineXZ(targetMeshFilter);
+            var worldPoints = targetMeshFilter.sharedMesh.vertices
+                .Select(v => targetMeshFilter.transform.TransformPoint(v))
+                .Select(p => new Vector3(p.x, 0, p.z))
+                .ToList();
+
+            var edgePoints = GenerateConcaveHullXZ(worldPoints, concavity, scaleFactor);
             var mesh = GenerateExtrudedMesh(edgePoints, yOffset, extrusion);
 
             GameObject container = new GameObject("Generated_Collider");
@@ -68,60 +82,55 @@ namespace Code.Editors
             Debug.Log("Collider mesh generated successfully.");
         }
 
-        private List<Vector3> ExtractMeshOutlineXZ(MeshFilter meshFilter)
+        private List<Vector3> GenerateConcaveHullXZ(List<Vector3> points, double concavity = 0.5f, 
+            double scaleFactor = 1.5f)
         {
-            var mesh = meshFilter.sharedMesh;
-            var vertices = mesh.vertices.Select(v => meshFilter.transform.TransformPoint(v)).ToArray();
-            var triangles = mesh.triangles;
-
-            var edgeCount = new Dictionary<(Vector3, Vector3), int>();
-
-            for (int i = 0; i < triangles.Length; i += 3)
+            Hull.CleanUp();
+            
+            List<Node> nodes = new();
+            for (int i = 0; i < points.Count; i++)
             {
-                Vector3[] triVerts =
-                {
-                    new Vector3(vertices[triangles[i]].x, 0, vertices[triangles[i]].z),
-                    new Vector3(vertices[triangles[i + 1]].x, 0, vertices[triangles[i + 1]].z),
-                    new Vector3(vertices[triangles[i + 2]].x, 0, vertices[triangles[i + 2]].z)
-                };
-
-                void AddEdge(Vector3 a, Vector3 b)
-                {
-                    var key = (a, b);
-                    var reverseKey = (b, a);
-                    if (edgeCount.ContainsKey(reverseKey)) edgeCount[reverseKey]++;
-                    else if (edgeCount.ContainsKey(key)) edgeCount[key]++;
-                    else edgeCount[key] = 1;
-                }
-
-                AddEdge(triVerts[0], triVerts[1]);
-                AddEdge(triVerts[1], triVerts[2]);
-                AddEdge(triVerts[2], triVerts[0]);
-            }
-
-            var boundaryEdges = edgeCount.Where(e => e.Value == 1).Select(e => e.Key).ToList();
-            var outline = new List<Vector3> { boundaryEdges[0].Item1, boundaryEdges[0].Item2 };
-            boundaryEdges.RemoveAt(0);
-
-            while (boundaryEdges.Count > 0)
-            {
-                var last = outline[^1];
-                int index = boundaryEdges.FindIndex(e => e.Item1 == last || e.Item2 == last);
-
-                if (index == -1)
-                {
-                    Debug.LogWarning("Unclosed loop detected. Ending outline early.");
-                    break;
-                }
-
-                var nextEdge = boundaryEdges[index];
-                outline.Add(nextEdge.Item1 == last ? nextEdge.Item2 : nextEdge.Item1);
-                boundaryEdges.RemoveAt(index);
+                var p = points[i];
+                nodes.Add(new Node(p.x, p.z, i));
             }
             
+            Hull.SetConvexHull(nodes);
+            Hull.SetConcaveHull(concavity, scaleFactor);
+            
+            var outline = new List<Vector3>();
+            var edges = Hull.HullConcaveEdges;
+
+            if (edges.Count == 0) 
+                return outline;
+            
+            var current = edges[0];
+            outline.Add(new Vector3((float)current.Nodes[0].X, 0, (float)current.Nodes[0].Y));
+            outline.Add(new Vector3((float)current.Nodes[1].X, 0, (float)current.Nodes[1].Y));
+
+            edges.RemoveAt(0);
+
+            while (edges.Count > 0)
+            {
+                var last = outline[^1];
+                int index = edges.FindIndex(e =>
+                    (float)e.Nodes[0].X == last.x && (float)e.Nodes[0].Y == last.z ||
+                    (float)e.Nodes[1].X == last.x && (float)e.Nodes[1].Y == last.z);
+
+                if (index == -1) 
+                    break;
+
+                var e = edges[index];
+                var next = ((float)e.Nodes[0].X == last.x && (float)e.Nodes[0].Y == last.z)
+                    ? e.Nodes[1]
+                    : e.Nodes[0];
+
+                outline.Add(new Vector3((float)next.X, 0, (float)next.Y));
+                edges.RemoveAt(index);
+            }
+
             return outline;
         }
-
+        
         private Mesh GenerateExtrudedMesh(List<Vector3> path, float height, float thickness)
         {
             var verts = new List<Vector3>();
@@ -162,7 +171,8 @@ namespace Code.Editors
 
             foreach (var mf in meshFilters)
             {
-                if (mf == null || mf.sharedMesh == null) continue;
+                if (mf == null || mf.sharedMesh == null) 
+                    continue;
 
                 var mesh = mf.sharedMesh;
                 var matrix = mf.transform.localToWorldMatrix;
